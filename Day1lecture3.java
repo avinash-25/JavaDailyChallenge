f50d9540cccc4d8e8ab9236b1c725d92dca8efd6
avinash-25 <avinashranjan268@gmail.com>
2023-09-30 15:34:03 +0530

    Z

--- Day1lecture3.java
+++ Day1lecture3.java
@@ -1,9 +1,15 @@
 public class Day1lecture3 {
     public static void main(String[] args) {
-        long l = 234242323;
-        double d = (10.0f/3.0f);
-        float f = (10.0f/3.0f);
-        System.out.println("d = "+d);
-        System.out.println("f = "+f);
+//        long l = 234242323;
+//        double d = (10.0f/3.0f);
+//        float f = (10.0f/3.0f);
+//        System.out.println("d = "+d);
+//        System.out.println("f = "+f);
+        boolean x = true;
+        if(x)
+            System.out.println("true part");
+        else
+            System.out.println("Else part");
+
     }
 }
