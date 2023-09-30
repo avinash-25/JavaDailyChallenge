122f68935aeb17bb4628c2fb10914c9808f46830
avinash-25 <avinashranjan268@gmail.com>
2023-09-30 12:51:48 +0530

    z

--- Day1Lecture2.java
+++ Day1Lecture2.java
@@ -1,3 +1,15 @@
 public class Day1Lecture2 {
+    public static void main(String[] args) {
+        byte b = -128;
+        byte a = 127;
+        byte min = Byte.MIN_VALUE;
+        byte max = Byte.MAX_VALUE;
+//        System.out.println("min = "+min);
+//        System.out.println("max = "+max);
+
+        int x = 2147483647;
+//        int y = 2147483648;
+        System.out.println(x);
+    }
 
 }
