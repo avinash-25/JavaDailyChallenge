8c7453cb5d7e759f91f1ad19d804f2ebf1c97a81
avinash-25 <avinashranjan268@gmail.com>
2023-09-30 11:33:15 +0530

    z

---
+++ Day1Lecture1.java
@@ -0,0 +1,13 @@
+public class Day1Lecture1 {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("x = "+x);
+        int String  = 500;
+        System.out.println("String = "+String);
+        float Runnable = 34.0f;
+        System.out.println("Runnable  :- "+Runnable);
+        int $ = 90;
+        int e234g = 900;
+        int _$_$_$_$_$_$_$ =  89898;
+    }
+}
